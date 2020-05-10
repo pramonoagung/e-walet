5de70d88caaec9424a682ac18a59c098ce6e455a
Agung Pramono <agung.prasatu@gmail.com>
2020-05-10 13:27:13 +0700

    first commit

---
+++ src/main/java/com/project/ewalet/EWaletApplication.java
@@ -0,0 +1,13 @@
+package com.project.ewalet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWaletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EWaletApplication.class, args);
+	}
+
+}
