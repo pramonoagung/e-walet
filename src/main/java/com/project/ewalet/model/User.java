e5e4795418dc97cc3eda798a272257561c6d637d
Agung Pramono <agung.prasatu@gmail.com>
2020-05-11 11:48:16 +0700

    Update token length

--- src/main/java/com/project/ewalet/model/User.java
+++ src/main/java/com/project/ewalet/model/User.java
@@ -31,7 +31,7 @@ public class User {
     private String last_name;
     @Column(length = 25)
     private String phone_number;
-    @Column(length = 100)
+    @Column(length = 255)
     private String token;
     @Column()
     private int status;
