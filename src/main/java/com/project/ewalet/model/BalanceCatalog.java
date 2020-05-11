23d6fb7b11593baadb326c882247f992545511f3
Habib Yafi Ardi <ardih@DNID0687L01.local>
2020-05-11 15:18:12 +0700

    add balance_catalog DAO

--- src/main/java/com/project/ewalet/model/BalanceCatalog.java
+++ src/main/java/com/project/ewalet/model/BalanceCatalog.java
@@ -5,14 +5,14 @@ import lombok.Data;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "balance_catalog")
+@Table(name = "balance_catalog", indexes = @Index(columnList = "id"))
 @Data
 public class BalanceCatalog {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
+    @Column(length = 25)
     private String code;
     private long balance;
 }
