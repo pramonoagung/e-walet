c0b3ba8829c1bb1c58fa2d6042f480062bd9b844
Agung Pramono <agung.prasatu@gmail.com>
2020-05-12 22:21:09 +0700

    Add bank package

---
+++ src/main/java/bank/BankApplication.java
@@ -0,0 +1,14 @@
+package bank;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApplication.class, args);
+	}
+
+}
---
+++ src/main/java/bank/Controller/BankApiController.java
@@ -0,0 +1,8 @@
+package bank.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BankApiController {
+
+}
