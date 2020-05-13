d40c3035fe39aaf8535da5784cc99e26a3c3a573
Agung Pramono <agung.prasatu@gmail.com>
2020-05-13 20:10:06 +0700

    Add resend otp

--- src/main/java/com/project/ewalet/controller/OtpController.java
+++ src/main/java/com/project/ewalet/controller/OtpController.java
@@ -2,6 +2,7 @@ package com.project.ewalet.controller;
 
 import com.project.ewalet.mapper.OtpMapper;
 import com.project.ewalet.model.payload.ResendOtpRequest;
+import com.project.ewalet.service.rabbitmq.MQPublisher;
 import com.project.ewalet.utils.Validation;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.DecimalFormat;
+import java.util.Random;
+
 @RestController
 @CrossOrigin
 public class OtpController {
@@ -20,12 +24,16 @@ public class OtpController {
     OtpMapper otpMapper;
     @Autowired
     Validation validation;
+    @Autowired
+    private MQPublisher mqPublisher;
 
     @PostMapping("/resend-otp")
     public ResponseEntity<?> resendOtp(@RequestBody ResendOtpRequest resendOtpRequest) {
         JSONObject jsonObject = new JSONObject();
+        String otpCode = new DecimalFormat("000000").format(new Random().nextInt(999999));
         if (validation.email(resendOtpRequest.getEmail()) && validation.phoneNumber(resendOtpRequest.getPhone_number())) {
-            
+            sendSms(resendOtpRequest.getPhone_number(), otpCode);
+            sendEmail(resendOtpRequest.getEmail(), otpCode);
             return new ResponseEntity<>(jsonObject, HttpStatus.OK);
         } else {
             jsonObject.put("status", 406);
@@ -34,4 +42,17 @@ public class OtpController {
         }
     }
 
+    void sendSms(String phoneNumber, String otpCode) {
+        JSONObject jsonSendSms = new JSONObject();
+        jsonSendSms.put("phoneNumber", phoneNumber);
+        jsonSendSms.put("otpCode", otpCode);
+        mqPublisher.mqSendSms(jsonSendSms.toString());
+    }
+
+    void sendEmail(String toEmail, String otpCode) {
+        JSONObject jsonSendEmail = new JSONObject();
+        jsonSendEmail.put("toEmail", toEmail);
+        jsonSendEmail.put("otpCode", otpCode);
+        mqPublisher.mqSendEmail(jsonSendEmail.toString());
+    }
 }
