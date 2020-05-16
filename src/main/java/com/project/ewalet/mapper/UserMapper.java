00ff0d66236c8740e7dc8f840d6115e3c2d7b537
Agung Pramono <agung.prasatu@gmail.com>
2020-05-16 23:40:15 +0700

    Update mappet syntax

--- src/main/java/com/project/ewalet/mapper/UserMapper.java
+++ src/main/java/com/project/ewalet/mapper/UserMapper.java
@@ -10,64 +10,62 @@ import java.util.List;
 @Mapper
 public interface UserMapper {
 
-    final String getAll = "SELECT * FROM user";
-    final String getById = "SELECT * FROM user WHERE ID = #{ID}";
-    final String getByName = "SELECT * FROM user WHERE NAME = #{name}";
-    final String deleteById = "DELETE from user WHERE ID = #{id}";
-    final String insert = "INSERT INTO user (NAME, BRANCH, PERCENTAGE, PHONE, EMAIL ) VALUES (#{name}, #{branch}, #{percentage}, #{phone}, #{email})";
+    final String getAll = "select * from user";
+    final String getById = "select * from user where id = #{id}";
+    final String getByName = "select * from user where name = #{name}";
+    final String deleteById = "delete from user where id = #{id}";
+    final String update = "update user set email = #{email}, password = #{password}, first_name = #{first_name}, " +
+            "last_name = #{last_name}, status = #{status}, phone_number = #{phone_number} where id = #{id}";
 
-    final String update = "UPDATE user SET EMAIL = #{email}, password = #{password}, first_NAME = #{first_name}, " +
-            "last_name = #{last_name}, status = #{status}, PHONE_number = #{phone_number} WHERE ID = #{id}";
+    final String login = "update user set login_status = true where id = #{id}";
+    final String logout = "update user set login_status = false where id = #{id}";
+    final String auth = "select * from user where email = #{email} and password = #{password}";
 
-    final String login = "UPDATE user SET LOGIN_STATUS = TRUE WHERE ID = #{id}";
-    final String logout = "UPDATE user SET LOGIN_STATUS = FALSE WHERE ID = #{id}";
-    final String auth = "SELECT * FROM user WHERE EMAIL = #{email} AND PASSWORD = #{password}";
-
-    final String findByPhoneNumber = "SELECT * FROM user WHERE PHONE_NUMBER = #{phone_number}";
-    final String findByEmail = "SELECT * FROM user WHERE EMAIL = #{email}";
-    final String findByEmailAndPhone = "SELECT * FROM user WHERE EMAIL = #{email} and phone_number = #{phone_number}";
-    final String save = "INSERT INTO user (EMAIL, PASSWORD, FIRST_NAME, LAST_NAME, PHONE_NUMBER, STATUS, CREATED_AT ) " +
-            "VALUES (#{email}, #{password}, #{first_name}, #{last_name}, #{phone_number}, #{status}, #{created_at})";
-    final String updateToken = "UPDATE user SET TOKEN = #{token} WHERE ID = #{id}";
+    final String findByPhoneNumber = "select * from user where phone_number = #{phone_number}";
+    final String findByEmail = "select * from user where email = #{email}";
+    final String findByEmailAndPhone = "select * from user where email = #{email} and phone_number = #{phone_number}";
+    final String save = "insert into user (email, password, FIRST_name, LAST_name, phone_number, status, created_at ) " +
+            "values (#{email}, #{password}, #{first_name}, #{last_name}, #{phone_number}, #{status}, #{created_at})";
+    final String updateToken = "update user set token = #{token} where id = #{id}";
 
     @Select(findByPhoneNumber)
     @Results(value = {
-            @Result(property = "id", column = "ID"),
-            @Result(property = "email", column = "EMAIL"),
-            @Result(property = "password", column = "PASSWORD"),
-            @Result(property = "first_name", column = "FIRST_NAME"),
-            @Result(property = "last_name", column = "LAST_NAME"),
-            @Result(property = "phone_number", column = "PHONE_NUMBER"),
-            @Result(property = "status", column = "STATUS"),
-            @Result(property = "created_at", column = "CREATED_AT")
+            @Result(property = "id", column = "id"),
+            @Result(property = "email", column = "email"),
+            @Result(property = "password", column = "password"),
+            @Result(property = "first_name", column = "first_name"),
+            @Result(property = "last_name", column = "last_name"),
+            @Result(property = "phone_number", column = "phone_number"),
+            @Result(property = "status", column = "status"),
+            @Result(property = "created_at", column = "created_at")
     })
     User findByPhoneNumber(String phone_number);
 
     @Select(findByEmail)
     @Results(value = {
-            @Result(property = "id", column = "ID"),
-            @Result(property = "email", column = "EMAIL"),
-            @Result(property = "password", column = "PASSWORD"),
-            @Result(property = "first_name", column = "FIRST_NAME"),
-            @Result(property = "last_name", column = "LAST_NAME"),
-            @Result(property = "phone_number", column = "PHONE_NUMBER"),
-            @Result(property = "token", column = "TOKEN"),
-            @Result(property = "status", column = "STATUS"),
-            @Result(property = "created_at", column = "CREATED_AT")
+            @Result(property = "id", column = "id"),
+            @Result(property = "email", column = "email"),
+            @Result(property = "password", column = "password"),
+            @Result(property = "first_name", column = "FIRST_name"),
+            @Result(property = "last_name", column = "LAST_name"),
+            @Result(property = "phone_number", column = "phone_number"),
+            @Result(property = "token", column = "token"),
+            @Result(property = "status", column = "status"),
+            @Result(property = "created_at", column = "created_at")
     })
     User findByEmail(String email);
 
     @Select(findByEmailAndPhone)
     @Results(value = {
-            @Result(property = "id", column = "ID"),
-            @Result(property = "email", column = "EMAIL"),
-            @Result(property = "password", column = "PASSWORD"),
-            @Result(property = "first_name", column = "FIRST_NAME"),
-            @Result(property = "last_name", column = "LAST_NAME"),
-            @Result(property = "phone_number", column = "PHONE_NUMBER"),
-            @Result(property = "token", column = "TOKEN"),
-            @Result(property = "status", column = "STATUS"),
-            @Result(property = "created_at", column = "CREATED_AT")
+            @Result(property = "id", column = "id"),
+            @Result(property = "email", column = "email"),
+            @Result(property = "password", column = "password"),
+            @Result(property = "first_name", column = "FIRST_name"),
+            @Result(property = "last_name", column = "LAST_name"),
+            @Result(property = "phone_number", column = "phone_number"),
+            @Result(property = "token", column = "token"),
+            @Result(property = "status", column = "status"),
+            @Result(property = "created_at", column = "created_at")
     })
     User findByEmailAndPhone(String email, String phone_number);
 
@@ -80,41 +78,41 @@ public interface UserMapper {
 
     @Select(getAll)
     @Results(value = {
-            @Result(property = "id", column = "ID"),
-            @Result(property = "email", column = "EMAIL"),
-            @Result(property = "password", column = "PASSWORD"),
-            @Result(property = "name", column = "NAME"),
-            @Result(property = "loginStatus", column = "LOGIN_STATUS")
+            @Result(property = "id", column = "id"),
+            @Result(property = "email", column = "email"),
+            @Result(property = "password", column = "password"),
+            @Result(property = "name", column = "name"),
+            @Result(property = "loginStatus", column = "login_status")
     })
     List<User> getAll();
 
     @Select(getById)
     @Results(value = {
-            @Result(property = "id", column = "ID"),
-            @Result(property = "email", column = "EMAIL"),
-            @Result(property = "password", column = "PASSWORD"),
-            @Result(property = "name", column = "NAME"),
-            @Result(property = "loginStatus", column = "LOGIN_STATUS")
+            @Result(property = "id", column = "id"),
+            @Result(property = "email", column = "email"),
+            @Result(property = "password", column = "password"),
+            @Result(property = "name", column = "name"),
+            @Result(property = "loginStatus", column = "login_status")
     })
     User getById(long id);
 
     @Select(getByName)
     @Results(value = {
-            @Result(property = "id", column = "ID"),
-            @Result(property = "email", column = "EMAIL"),
-            @Result(property = "password", column = "PASSWORD"),
-            @Result(property = "name", column = "NAME"),
-            @Result(property = "loginStatus", column = "LOGIN_STATUS")
+            @Result(property = "id", column = "id"),
+            @Result(property = "email", column = "email"),
+            @Result(property = "password", column = "password"),
+            @Result(property = "name", column = "name"),
+            @Result(property = "loginStatus", column = "login_status")
     })
     User getByName(String name);
 
     @Select(auth)
     @Results(value = {
-            @Result(property = "id", column = "ID"),
-            @Result(property = "email", column = "EMAIL"),
-            @Result(property = "password", column = "PASSWORD"),
-            @Result(property = "name", column = "NAME"),
-            @Result(property = "loginStatus", column = "LOGIN_STATUS")
+            @Result(property = "id", column = "id"),
+            @Result(property = "email", column = "email"),
+            @Result(property = "password", column = "password"),
+            @Result(property = "name", column = "name"),
+            @Result(property = "loginStatus", column = "login_status")
     })
     User authValidation(String email, String password);
 
@@ -129,10 +127,6 @@ public interface UserMapper {
 
     @Delete(deleteById)
     void delete(int id);
-
-    @Insert(insert)
-    @Options(useGeneratedKeys = true, keyProperty = "id")
-    void insert(User user);
 
 }
 
